8e68b0a222d9e53cab0b3568d130bd4a43b25082
SawadaSaiko <sawarafael@gmail.com>
2019-09-12 01:04:40 -0300

    Roteamento configurado corretamente; Sistema de Calculo configurado/ ainda em desenvolvimento no código;

--- README.md
+++ README.md
@@ -7,22 +7,15 @@ Aqui será implementado todas as funções visuais que serão entregues ao usuá
 
 ## Lado Servidor
 
-Aqui será implementado todas as funções operacionais que serão entregues ao usuário por meio de uma conexão ao lado cliente. Antes de inicializar a codificação neste lado, é preciso instalar as suas dependências por meio do NodeJS:
+Aqui será implementado todas as funções operacionais que serão entregues ao usuário por meio de uma conexão ao lado cliente.
 
-*Comando: **"express install"** na pasta do aplicativo.*
+Antes de inicializar a codificação neste lado, é preciso instalar as suas dependências por meio do NodeJS:
 
-*Comando: **"nodemon install"** na pasta do aplicativo*
 
-a) **ExpressJS** - Abra o Prompt e na pasta que o repositório foi clonado: npm install express
-
-b) **Nodemon** - É um executor de testes, apenas inicialize o arquivo por meio do comando, no prompt, com **"nodemon server.js"**, que ele ficará atualizando pelo prompt/terminal o que há no servidor sempre que um save for executado.
+a) ExpressJS - Abra o Prompt e na pasta que o repositório foi clonado: npm install express
  
 
 Deploys:
 
 
-**05/09/2019** => Criação do Servidor para trabalhar em sua conectividade.
-
-**09/09/2019** => Servidor configurado para atender uma rota inicial.
-
-**10/09/2019** => Servidor com roteamento pronto; Arquitetura do Sistema definido para operacionalizar corretamente.
+05/09/2019 => Criação do Servidor para trabalhar em sua conectividade.
--- app/server.js
+++ app/server.js
@@ -1,29 +1,81 @@
-const http      = require("http");
-const express   = require("express");
-var path        = require('path');
+const express    = require("express");
+const bodyParser = require('body-parser')
+
+const app = express();
 
 const port = 3000
 const ip = 'localhost';
 
-const app = express();
 
-app.get("/", function(req, res) {
+// BODY-PARSER PROPS
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 
-    //Esta será uma resposta que enviará ao navegador, uma página
-    //Se rodar como está agora com esses ???? = retornará erro na página
-    res.sendFile(path.join(__dirname + '/public/index.html'))
+// EXPRESS GETS e POSTS
+//Mostrar page - HOME
+app.get("/home", function(req, res) {
+
+    res.sendFile(__dirname + '/public/index.html')
 
 });
 
-app.listen(port, ip, () => {
+//Mostrar page - INFORMAÇÕES
+app.get("/info", function(req, res) {
 
-    console.log("#########################################")
-    console.log(`Servidor rodando em http://${ip}:${port}`)
-    console.log('Para derrubar o servidor: ctrl + c')
-    console.log("#########################################")
+    res.sendFile(__dirname + '/public/info.html')
+
+});
+
+//Rota que calcula a Glicose com Carboidrato e retorna o valor da Insulina
+app.post("/calc", function(req, res) {
+
+    var glico = req.body.glicose;
+    var carbo = req.body.carboidratos;
+    
+    var insulina;
+    
+    insulina = glico / carbo;
+
+    var resultado = Math.round(insulina)
+
+        if (resultado > 10) {
+            res.send(`Vá em um posto médico!`)
+        } else {
+            res.send('<h1>'+resultado+"<h1>")
+        }       
 
 })
+
+
+
+//Mostrar page - DISPONIBILIDADE DE CENTROS DE SAÚDE
+app.get("/dispo", function(req, res) {
+
+    res.sendFile(__dirname + '/public/dispo.html')
+
+});
+
+
+
+
+
+
+
+app.listen(port, ip, () => {
+
+    console.log("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
+    console.log("■                 Servidor Inicializado!!                    ■")
+    console.log(`■ 1) Para Acessar o Servidor: http://${ip}:${port}/home     ■`)
+    console.log("■ 2) Para Derrubar o Servidor: ctrl + c                      ■")
+    console.log("■ 3) Para Reiniciar o Servidor: rs                           ■")
+    console.log("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
+
+})
+
+
+
+
 
 
 
